79b2be3b5ef5bc13be1464c7de4b4e63f0c1cae6
Giuseppe <mgem2@hotmail.it>
2025-07-13 23:05:50 +0200

    fix

--- dist/routes/auth.routes.js
+++ dist/routes/auth.routes.js
@@ -3,19 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // src/routes/auth.routes.ts
 const express_1 = require("express");
 const auth_controller_1 = require("../controllers/auth.controller");
+const auth_middleware_1 = require("../middlewares/auth.middleware");
 const router = (0, express_1.Router)();
-router.post('/register', auth_controller_1.register);
-router.post('/login', auth_controller_1.login);
-router.post('/2fa/setup', auth_controller_1.enable2FA);
-router.post('/2fa/verify', auth_controller_1.verify2FA);
-router.get("/2fa/status", auth_controller_1.get2FAStatus);
-router.get("/2fa/generate", auth_controller_1.generate2FASecret);
-router.post("/2fa/disable", auth_controller_1.disable2FA);
-/*
-router.post('/2fa/setup', authenticate, enable2FA);
-router.post('/2fa/verify', authenticate, verify2FA);
-router.get("/2fa/status", authenticate, get2FAStatus);
-router.get("/2fa/generate", authenticate, generate2FASecret);
-router.post("/2fa/disable", authenticate, disable2FA);
-*/
+router.post('/2fa/setup', auth_middleware_1.authenticate, auth_controller_1.enable2FA);
+router.post('/2fa/verify', auth_middleware_1.authenticate, auth_controller_1.verify2FA);
+router.get("/2fa/status", auth_middleware_1.authenticate, auth_controller_1.get2FAStatus);
+router.get("/2fa/generate", auth_middleware_1.authenticate, auth_controller_1.generate2FASecret);
+router.post("/2fa/disable", auth_middleware_1.authenticate, auth_controller_1.disable2FA);
 exports.default = router;
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -6,19 +6,13 @@ import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/2fa/setup', enable2FA);
-router.post('/2fa/verify',  verify2FA);
-router.get("/2fa/status", get2FAStatus);
-router.get("/2fa/generate", generate2FASecret);
-router.post("/2fa/disable", disable2FA);
 
-/*
+
+
 router.post('/2fa/setup', authenticate, enable2FA);
 router.post('/2fa/verify', authenticate, verify2FA);
 router.get("/2fa/status", authenticate, get2FAStatus);
 router.get("/2fa/generate", authenticate, generate2FASecret);
 router.post("/2fa/disable", authenticate, disable2FA);
-*/
+
 export default router;
