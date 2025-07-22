4782e0c48237d03cd59383e693c21c7669c6700f
Giuseppe <mgem2@hotmail.it>
2025-07-23 00:55:09 +0200

    remove dist

--- dist/utils/prisma.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client"); // Adjust the import path based on your project structure
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
