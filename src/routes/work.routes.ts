8c92c1048dfd999291981d313a4527f484398626
Giuseppe <mgem2@hotmail.it>
2025-08-08 23:59:27 +0200

    getUnpaidWorks

--- src/routes/work.routes.ts
+++ src/routes/work.routes.ts
@@ -1,12 +1,13 @@
 // src/routes/service.routes.ts
 import { Router } from 'express';
-import { createWork, getAllWorks, getWorkById, updateWork, deleteWork } from '../controllers/work.controller';
+import { createWork, getAllWorks, getWorkById, updateWork, deleteWork, getUnpaidWorks } from '../controllers/work.controller';
 import { authenticate, authorizeRole, hasPermission } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 router.post('/', authenticate, authorizeRole(['Admin', 'Manager', 'User']), hasPermission('create:service'), createWork);
 router.get('/', authenticate, authorizeRole(['Admin', 'Manager', 'User']), hasPermission('read:service'), getAllWorks);
+router.get('/unpaid', authenticate, authorizeRole(['Admin', 'Manager', 'User']), hasPermission('read:service'), getUnpaidWorks);
 router.get('/:id', authenticate, authorizeRole(['Admin', 'Manager', 'User']), hasPermission('read:service'), getWorkById);
 router.put('/:id', authenticate, authorizeRole(['Admin', 'Manager', 'User']), hasPermission('update:service'), updateWork);
 router.delete('/:id', authenticate, authorizeRole(['Admin']), hasPermission('delete:service'), deleteWork);
